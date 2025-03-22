9c1e56fda2e33944a81ca3e37d3eb252de205236
LievreVolant <156901075+LievreVolant@users.noreply.github.com>
2025-03-23 00:01:42 +0100

    Correction petit bug bouton creer

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ import { Alchimix } from './model.js';
 import { view } from './view.js';
 
 let alchimix = new Alchimix();
+let listIngredient = []
 
 // Récupération des données du LocalStorage
 alchimix.retrieveStateFromClient();
@@ -65,15 +66,101 @@ view.btnFav.addEventListener("click", function(event) {
     });
     
   }
+});
 
-  view.btnCreate.addEventListener("click", function(event) {
+view.btnCreate.addEventListener("click", function(event) {
 
-    view.imageCrea.src = "images/crea" + Math.floor(Math.random() * 4) + ".png"
+  if (listIngredient.length != 0) {
+    if (false) { // insérer condition cocktail existant
+      // insérer image cocktail existant
+    } else {
+      view.imageCrea.src = "images/crea" + Math.floor(Math.random() * 4) + ".png"
+    }
     view.dialogCrea.showModal();
 
     view.btnFermerCrea.addEventListener("click", function(event) {
       view.dialogCrea.close();
     })
-  })
+  }
+});
 
+
+// ### Partie DragNDrop
+let currentDroppable = null;
+
+document.addEventListener('mousedown', function(event) {
+
+  let dragElement = event.target.closest('.ingredient');
+
+    if (dragElement != null) {
+      dragElement.onmousedown = function(event) {
+
+        // On corrige la distance entre le pointeur de la souris et le centre de l'ingrédient
+        let shiftX = event.clientX - dragElement.getBoundingClientRect().left;
+        let shiftY = event.clientY - dragElement.getBoundingClientRect().top;
+  
+        dragElement.style.position = 'absolute';
+        dragElement.style.zIndex = 1000;
+        document.body.append(dragElement);
+  
+        moveAt(event.pageX, event.pageY);
+  
+        function moveAt(pageX, pageY) {
+          dragElement.style.left = pageX - shiftX + 'px';
+          dragElement.style.top = pageY - shiftY + 'px';
+        }
+  
+        function onMouseMove(event) {
+          moveAt(event.pageX, event.pageY);
+  
+          dragElement.hidden = true;
+          let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+          dragElement.hidden = false;
+  
+          if (!elemBelow) return;
+  
+          let droppableBelow = elemBelow.closest('.droppable');
+          if (currentDroppable != droppableBelow) {
+            if (currentDroppable) {
+              leaveDroppable(currentDroppable);
+            }
+            currentDroppable = droppableBelow;
+            if (currentDroppable) {
+              enterDroppable(currentDroppable);
+            }
+          }
+        }
+  
+        document.addEventListener('mousemove', onMouseMove);
+  
+        dragElement.onmouseup = function() {
+          document.removeEventListener('mousemove', onMouseMove);
+          dragElement.onmouseup = null;
+        };
+  
+      };
+
+      // Quand un ingrédient est glissé dans la zone du verre
+    function enterDroppable(elem) {
+      // On change la couleur du fond
+      elem.style.background = '#CA5D36';
+      // Si l'ingrédient n'est pas déjà présenrt dans la liste on l'y ajoute
+      if(!listIngredient.includes(dragElement.id)) {
+        listIngredient.push(dragElement.id);
+      }
+    }
+
+    // Quand un ingrédient est glissé hors de la zone du verre
+    function leaveDroppable(elem) {
+      // On enlève la couleur du fond
+      elem.style.background = ''
+      // On retire l'ingrédient de la liste
+      listIngredient = listIngredient.filter((ing) => ing !== dragElement.id)
+    }
+
+    dragElement.ondragstart = function() {
+      return false;
+    };
+
+  }
 });
