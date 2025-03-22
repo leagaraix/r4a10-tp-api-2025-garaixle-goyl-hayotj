1a3af71706a7a5ddfe8339c6e63a7b16bea877a5
Léa Garaix <lea.garaix@etu.univ-grenoble-alpes.fr>
2025-03-22 11:50:52 +0100

    Ajout images, recherches sur ajout favoris.

--- js/view.js
+++ js/view.js
@@ -11,4 +11,8 @@ export const view = {
 
   // Bouton de recherche
   rechercheButton: document.getElementById("btn-lancer-recherche"),
+
+  // Bouton favoris (recherche)
+  // favorisButton: document.getElementById("btn-ajouter-favori"),
+  
 };
