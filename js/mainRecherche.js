bcac18e8b05f8eb14f2ae7e85a0920a50ea5b3fa
Léa Garaix <lea.garaix@etu.univ-grenoble-alpes.fr>
2025-03-24 14:22:35 +0100

    Détails style

--- js/mainRecherche.js
+++ js/mainRecherche.js
@@ -28,7 +28,7 @@ viewRecherche.rechercheButton.addEventListener("click", async (evt) => {
 
     //On met à jour la vue
     if((dataByName.drinks == null && dataByIngredient.drinks == "no data found")  || (dataByName.drinks == "no data found" && dataByIngredient.drinks == "no data found") || (dataByName == undefined && dataByIngredient.drinks == undefined)){
-        viewRecherche.resultatContainer.innerHTML = "<div><p>Aucun cocktail n'a été trouvé</p></div>"
+        viewRecherche.resultatContainer.innerHTML = "<p id='aucun-resultat'>Aucun cocktail ne correspond à votre recherche.</p>"
     }else{
         //On remet le champ vide
         viewRecherche.resultatContainer.innerHTML = "";
